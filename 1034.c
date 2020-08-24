48b0d87cbd1e4abc4d7b4bc36328dca36bc06ff8
Sangmeeeee <goodman1223@naver.com>
2020-08-24 19:09:43 +0900

    Create 1034.c

---
+++ 1034.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+//1번문제
+char input[52][52];
+char count[52];
+int main()
+{
+	int raw, col, k, max = 0;
+	int zero_count;
+	scanf("%d %d", &raw, &col);
+	for (int i = 0; i < raw; i++) {
+		scanf("%s", input[i]);
+		for (int j = 0; j <= i; j++)
+			if (strcmp(input[i], input[j]) == 0) {
+				count[j]++;
+				break;
+			}
+	}
+	scanf("%d", &k);
+
+	for (int i = 0; i < raw; i++)
+	{
+		zero_count = 0;
+		if (count[i] != 0) 
+		{
+			for (int j = 0; j < col; j++)
+				if (input[i][j] == '0')
+					zero_count++;
+			if (k % 2 == zero_count % 2 && zero_count <= k)
+				if (max < count[i])
+					max = count[i];
+		}
+	}
+	printf("%d", max);
+}
